7f158173c7846ef971f3f2b2cf8497ae26dff88e
S1M0N21 <s1m0n21@hotmail.com>
2021-04-01 17:07:45 +0800

    add OptLevelFromEnv

--- opts.go
+++ opts.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
 )
 
 type Option func(*config)
@@ -21,5 +22,13 @@ func OptSetLogFile(filename string, size, backups, age int) Option {
 func OptSetLevel(lvl string) Option {
 	return func(c *config) {
 		c.lvl = getLevel(lvl)
+	}
+}
+
+func OptLevelFromEnv() Option {
+	return func(c *config) {
+		if lvl, set := os.LookupEnv("GO_LOG_LEVEL"); set {
+			c.lvl = getLevel(lvl)
+		}
 	}
 }
